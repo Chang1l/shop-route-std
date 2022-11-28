ed6fb42b6079be981b013f18f40b4f106dcfc4aa
DESKTOP-QT4Q3E1\PC-17 <4718794@naver.com>
2022-11-28 18:19:38 +0900

    11-28

--- src/Details.js
+++ src/Details.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import {useState} from 'react';
-import {useParams} from 'react-router-dom';
-
+import {useParams,useNavigate} from 'react-router-dom';
+import {Button,Container,CustomBtn}from './StyleCss';
 export const Details=(props)=>{
 let[likecnt, likecntchk]=useState(0);
 let {pid, ln}=useParams();
 if(pid>ln)pid=0;
+let nav= useNavigate();
+let pidImg=pid>ln? 0:pid;
 
-let divStyle={backgroundImage:'url('+process.env.PUBLIC_URL+'/img/jp'+pid+'.png'};
+let divStyle={backgroundImage:'url('+process.env.PUBLIC_URL+'/img/jp'+pidImg+'.png'};
 
 return(
-    <div className="MainImgDiv">
+        <Container>
         <div className="imgBox">
             <div className="imgCon" alt={'이미지'+pid} style={divStyle} />
              
@@ -18,7 +20,10 @@ return(
             <p>상품{pid} 설명</p>
             <p className="lkcnt" onClick={()=> likecntchk(++likecnt)}>좋아요{likecnt}</p> 
            </div>
-    </div>
+            <Button primary onClick={()=>nav(-1)}>뒤로가기 </Button><br/>
+            <CustomBtn bg={'#000'} cl={'#fff'}>검은색 버튼</CustomBtn>
+            <CustomBtn bg={'#f00'} cl={'#fff'}>빨간색 버튼</CustomBtn>
+           </Container>
 
 );
 
