99b73e8557869a0d888514763acc8e1bb5649eb9
DESKTOP-QT4Q3E1\PC-17 <4718794@naver.com>
2022-11-29 16:20:05 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -3,21 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from'./store.js';
+
+import {BrowserRouter} from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<React.StrictMode>
-{/* 수정시작 */}
-    <Provider store={store}>
-        <BrowserRouter>
-					<App/>
-        </BrowserRouter>
-{/* 수정끝 */}
-    </Provider>
-</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
